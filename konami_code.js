c3d06fdb2b178dd0382083f60d099a091937bb30
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-03-27 22:24:03 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,23 +2,23 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let tracker = 0
 function init(){
-function onKeyDown(event){
-  let key = parseInt(event.which)
-  console.log(event.which);
+  function onKeyDown(event){
+   let key = parseInt(event.which)
+    console.log(event.which);
   
-  if (key === code[tracker]){
+    if (key === code[tracker]){
       tracker++
-  }
-  if (tracker === code.length){
+   }
+   if (tracker === code.length){
       alert("Nice Job!")
       tracker = 0;
 
-  } else{
-  tracker = 0;
+   } else{
+   tracker = 0;
+   }
   }
-}
 
-document.addEventListener('keydown', onKeyDown)
+  document.addEventListener('keydown', onKeyDown)
 }
 
 init()
