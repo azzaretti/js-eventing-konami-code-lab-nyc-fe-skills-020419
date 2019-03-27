361745fab6f0ce1be94f30daba24cacb67d4ea05
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-03-27 22:07:48 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let tracker = 0
 
-function keyDownKonami(){
+function init(){
   let key = parseInt(event.which)
   console.log(event.which);
   
@@ -23,6 +23,6 @@ function keyDownKonami(){
 // }
 
 
-document.addEventListener('keydown', keyDownKonami)
+document.addEventListener('keydown', init)
 
 
