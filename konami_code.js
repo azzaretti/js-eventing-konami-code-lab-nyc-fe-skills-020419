074c65fa1b22439542027bfbbb02d2744be8086a
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-03-27 22:24:27 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let tracker = 0
 function init(){
-  function onKeyDown(event){
+  function onKeyDown(){
    let key = parseInt(event.which)
     console.log(event.which);
   
